c1f36d9a22968493763c8c0d12785331d3003b54
Susant Paudel <techbizznepal@gmail.com>
2025-10-08 23:55:08 +0545

    wip order

--- src/modules/order.management/actions/actionGetOrders.ts
+++ src/modules/order.management/actions/actionGetOrders.ts
@@ -1,28 +1,25 @@
-import { ApiResponse, IMetaData, TURLSearchParams } from "@/modules/core";
+"use server";
+
 import { authAxiosInstance } from "@/modules/core/lib/utils.axios";
 import { handleUnknownError } from "@/modules/core/lib/utils.index";
 import { ORDER_MANAGEMENT_ROUTES } from "@/modules/order.management/routes";
-import { TProductIndexPayload } from "@/modules/product.management";
 
-export const actionGetOrders = async (
-  params?: TURLSearchParams,
-): Promise<TProductIndexPayload | IMetaData> => {
+export const actionGetOrders = async (formData: FormData) => {
   try {
+    const searchParams = new URLSearchParams(
+      formData as unknown as Record<string, string>,
+    );
     const client = await authAxiosInstance();
     const response = await client.get(
-      ORDER_MANAGEMENT_ROUTES.order.index.path,
-      {
-        params,
-      },
+      [ORDER_MANAGEMENT_ROUTES.order.index.path, searchParams].join("?"),
     );
 
-    console.log("responseData: ", response.data);
-    const responseData = response.data as ApiResponse<TProductIndexPayload>;
+    const responseData = response.data; //as ApiResponse<TProductIndexPayload>;
 
     if (responseData.metaData.error) {
       return { error: responseData.metaData.error };
     }
-    console.log("success fetch orders: ", responseData.data);
+    console.log("success fetch orders: ", responseData.data.payload.orders);
     return responseData.data.payload;
   } catch (error) {
     console.log("error fetch orders: ", error);
--- src/modules/order.management/components/client/order/Index.tsx
+++ src/modules/order.management/components/client/order/Index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,15 +11,18 @@ import {
 import { Table } from "@/components/ui/table";
 import PageContainer from "@/modules/core/components/server/PageContainer";
 import { actionGetOrders } from "@/modules/order.management/actions/actionGetOrders";
+import { useState } from "react";
 
 export default function Index() {
-  actionGetOrders()
-    .then((orders) => {
-      console.log(orders);
-    })
-    .catch((error) => {
-      console.log("error on component fetching orders: ", error);
-    });
+  const [orderList, setOrderList] = useState([]);
+  // actionGetOrders()
+  //   .then((orders) => {
+  //     console.log(orders);
+  //   })
+  //   .catch((error) => {
+  //     console.log("error on component fetching orders: ", error);
+  //   });
+
   return (
     <PageContainer pageTitle="Manage Orders">
       <Card>
@@ -28,51 +33,35 @@ export default function Index() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form action={actionGetOrders}>
             <div className="flex flex-col space-y-4">
               <div className="flex flex-row space-x-4 w-auto">
-                <div className="">
-                  <label htmlFor="status" className="block text-sm font-medium">
-                    Payment Method
-                  </label>
-                  <select
-                    id="status"
-                    name="status"
-                    className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  >
-                    <option>All</option>
-                    <option>COD</option>
-                    <option>WALLET</option>
-                  </select>
-                </div>
-                <div className="">
-                  <label htmlFor="status" className="block text-sm font-medium">
-                    Status
-                  </label>
-                  <select
-                    id="status"
-                    name="status"
-                    className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  >
-                    <option>All</option>
-                    <option>Pending</option>
-                    <option>Processing</option>
-                    <option>Shipped</option>
-                    <option>Delivered</option>
-                    <option>Cancelled</option>
-                  </select>
-                </div>
-                <div className="">
-                  <label htmlFor="date" className="block text-sm font-medium">
-                    Placed Date
-                  </label>
-                  <input
-                    type="date"
-                    id="date"
-                    name="date"
-                    className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <FilterDropdown
+                  label="Payment Method"
+                  id="payment_method"
+                  options={[
+                    { id: "all", label: "All", value: "all" },
+                    { id: "cod", label: "COD", value: "cod" },
+                    { id: "wallet", label: "WALLET", value: "wallet" },
+                  ]}
+                />
+                <FilterDropdown
+                  label="Status"
+                  id="status"
+                  options={[
+                    { id: "all", label: "All", value: "all" },
+                    { id: "pending", label: "Pending", value: "pending" },
+                    {
+                      id: "processing",
+                      label: "Processing",
+                      value: "processing",
+                    },
+                    { id: "shipped", label: "Shipped", value: "shipped" },
+                    { id: "delivered", label: "Delivered", value: "delivered" },
+                    { id: "cancelled", label: "Cancelled", value: "cancelled" },
+                  ]}
+                />
+                <DateFilter label="Placed Date Range" />
               </div>
               <div className="flex flex-row space-x-4 w-auto">
                 <Button variant="default">Filter</Button>
@@ -94,4 +83,57 @@ export default function Index() {
 
 function TableFilterSection() {
   return <div></div>;
+}
+
+function FilterDropdown({
+  id,
+  label,
+  options,
+}: {
+  id: string;
+  label: string;
+  options: { id: string; label: string; value: string }[];
+}) {
+  return (
+    <div className="flex flex-col items-center">
+      <label htmlFor="status" className="block text-sm font-medium">
+        {label}
+      </label>
+      <select
+        id={id}
+        name={id}
+        className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      >
+        {options.map((option) => (
+          <option key={option.id} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+function DateFilter({ label }: { label: string }) {
+  return (
+    <fieldset className="flex flex-row text-center">
+      <legend className="block text-sm font-medium">{label}</legend>
+      <div className="flex flex-row space-x-2 justify-items-center items-center">
+        <label>From</label>
+        <input
+          type="date"
+          id="from"
+          name="from"
+          className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+        <br />
+        <label>To</label>
+        <input
+          type="date"
+          id="to"
+          name="to"
+          className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </fieldset>
+  );
 }
