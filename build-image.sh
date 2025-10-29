e76192cc5711948f79be1a3ba109e0907f077580
Susant Paudel <techbizznepal@gmail.com>
2025-10-30 01:46:08 +0545

    update sh

--- build-image.sh
+++ build-image.sh
@@ -5,3 +5,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGE_NAME="techbizz/vendor:latest"
 docker buildx build --platform linux/amd64 -t "$IMAGE_NAME" "$SCRIPT_DIR"
 echo "Docker image '$IMAGE_NAME' built successfully."
+
+# if argument is given as push push to docker hub
+if [ "$1" == "push" ]; then
+  docker push "$IMAGE_NAME"
+  echo "Docker image '$IMAGE_NAME' pushed to Docker Hub successfully."
+fi
