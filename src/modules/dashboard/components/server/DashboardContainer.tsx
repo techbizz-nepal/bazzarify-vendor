03f0c7d32fdcc0c11ad48a8ae78785fb55696d0d
Susant Paudel <techbizznepal@gmail.com>
2025-11-16 05:35:00 +0545

    chore: validation for address

--- src/modules/dashboard/components/server/DashboardContainer.tsx
+++ src/modules/dashboard/components/server/DashboardContainer.tsx
@@ -16,44 +16,47 @@ import {
 } from "@/components/ui/sheet";
 import PageContainer from "@/modules/core/components/server/PageContainer";
 import SetBusinessAndEmailForm from "@/modules/guest/components/client/registration/SetBusinessAndEmailForm";
+import { headers } from "next/headers";
 
-export default function DashboardContainer() {
+export default async function DashboardContainer() {
+  const host = (await headers()).get("host") || "";
+  console.log(host);
   return (
     <PageContainer pageTitle={"Dashboard"}>
       <Statistics />
+      {host.startsWith("vendor.") && (
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-center text-destructive">
+              Due Work !!!
+            </CardTitle>
+            <CardDescription className="text-center text-destructive">
+              You have not set your Store Information.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="default">Update Store</Button>
+              </SheetTrigger>
+
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Set Business and Email</SheetTitle>
+                  <SheetDescription>short description</SheetDescription>
+                </SheetHeader>
+                <SetBusinessAndEmailForm />
+              </SheetContent>
+            </Sheet>
+          </CardContent>
+        </Card>
+      )}
     </PageContainer>
   );
 }
 
 const Statistics = () => (
-  <>
-    <Card className="items-center justify-center px-4" id="stats">
-      <p>Welcome to dashboard.</p>
-    </Card>
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-center text-destructive">
-          Due Work !!!
-        </CardTitle>
-        <CardDescription className="text-center text-destructive">
-          You have not set your Store Information.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="default">Update Store</Button>
-          </SheetTrigger>
-
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Set Business and Email</SheetTitle>
-              <SheetDescription>short description</SheetDescription>
-            </SheetHeader>
-            <SetBusinessAndEmailForm />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
-  </>
+  <Card className="items-center justify-center px-4" id="stats">
+    <p>Welcome to dashboard.</p>
+  </Card>
 );
